71be1697f789e741aaf194951123d68b21836141
Marcos <marcos.sob@hotmail.com>
2020-08-27 13:21:24 +0200

    add initialize for radar data

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "Eigen/Dense"
 #include "tools.h"
+#include <cmath>
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
@@ -77,7 +78,22 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
       // TODO: Convert radar from polar to cartesian coordinates 
       //         and initialize state.
+      double rho = measurement_pack.raw_measurements_(0);
+      double phi = measurement_pack.raw_measurements_(1);
+      double drh = measurement_pack.raw_measurements_(2);
 
+      while (phi > M_PI) phi -= (2*M_PI);
+      while (phi < M_PI) phi += (2*M_PI);
+
+      ekf_.x_(0) = rho * cos(phi);
+      ekf_.x_(1) = rho * sin(phi);
+      ekf_.x_(2) = drh * cos(phi);
+      ekf_.x_(0) = drh * sin(phi);
+
+      ekf_.P_(0,0) = R_radar_(0,0);
+      ekf_.P_(1,1) = R_radar_(0,0);
+      ekf_.P_(2,2) = R_radar_(2,2)*10.0;
+      ekf_.P_(3,3) = R_radar_(2,2)*10.0;
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       // TODO: Initialize state.
